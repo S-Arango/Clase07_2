7b4237b22d1113bfa6d25cb1552f29a0f498b0cb
ESTUDIANTE <ESTUDIANTE@DESKTOP-KTO51U0>
2018-02-07 08:49:42 -0500

    Clase 07_02 Ejercicio 2

--- src/unal/poo/practica/RobotBase.java
+++ src/unal/poo/practica/RobotBase.java
@@ -14,47 +14,61 @@ public class RobotBase
         
 	public static void main (String[] args){
             //Declarar la creacion de la ciudad
-            objetos = new City("Field.txt");
+            objetos = new City("Ejercicio2.txt");
 	    objetos.showThingCounts(true);
             
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,0, 2, Direction.EAST,10);
+            estudiante = new Robot(objetos,1, 3, Direction.WEST,10);
             
 	    //Mover una interseccion en el sentido al cual este apuntando el objeto.
-            estudiante.move ();
+            //estudiante.move ();
             
             //Girar a la izquierda
-            estudiante.turnLeft();
+            //estudiante.turnLeft();
             
             //Tomando decisiones, Si puedo tomar un Thing
-            boolean puedeTomar = estudiante.canPickThing();
+            //boolean puedeTomar = estudiante.canPickThing();
             
             //Tomar un Thing
-            if(puedeTomar == true)
-               estudiante.pickThing();
+            //if(puedeTomar == true)
+              // estudiante.pickThing();
             
             //Especifica el numero de Thing que tiene en robot en el bolso
-            int numeroThings = estudiante.countThingsInBackpack();
+            //int numeroThings = estudiante.countThingsInBackpack();
             
             //Poner Thing, se debe validar que tenga things en el bolso
-            estudiante.putThing();
+            //estudiante.putThing();
                        
             //Si el frente esta libre de Wall
-            estudiante.frontIsClear();
+            //estudiante.frontIsClear();
             
             //Invocando una funcion
-            creacionFuncion(4);
-            
+            creacionGirar(2);
+            creacionMover(1); 
+            creacionCorte(3);
+            creacionMover(2);
+            creacionGirar(2);
             //Toman un Thing
-            estudiante.pickThing();
+            //estudiante.pickThing();
             
             
 	}
+        public static void creacionCorte(int parametroEntrada){
+            for (int i = 0; i < parametroEntrada; i ++){
+                creacionGirar(3);
+                creacionMover(3);
+            }
+            creacionGirar(3);
+        }
         
-        public static void creacionFuncion(int parametroEntrada){
+        public static void creacionMover(int parametroEntrada){
             for (int i = 0; i < parametroEntrada; i++) 
                 estudiante.move();
+        }
+        public static void creacionGirar(int parametroEntrada){
+            for (int i = 0; i < parametroEntrada; i++) 
+                estudiante.turnLeft();
         }
 }
 
